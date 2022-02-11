b342b9e843472203dc300a2ac36c7217cdd23ea4
snthsh <26085932+snthsh@users.noreply.github.com>
2022-02-11 18:16:26 +0000

    add eslint and remove consoles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import _ from 'lodash';
 import axios from 'axios';
-//Constants
+// Constants
 import { BUTTONS_ARRAY, TOTAL_QUESTIONS } from './constants';
-//Styles
+// Styles
 import { GlobalStyle } from './App.styles';
-//Components
+// Components
 import Questions from './components/screens/Questions';
 import Home from './components/screens/Home';
 import Score from './components/screens/Score';
-//Utils
+// Utils
 import { sortArray, capitalCaseByWords } from './utils';
-//Types
+// Types
 import {
   QuizData,
   Question,
@@ -21,6 +21,7 @@ import {
 } from './types/Types';
 
 const App = () => {
+  const [isActivitySelected, setIsActivitySelected] = useState<boolean>(false);
   const [screen, setScreen] = useState<Screen>('HOME');
   const [loading, setLoading] = useState<boolean>(false);
   const [quizData, setQuizData] = useState<QuizData | any>({});
@@ -31,16 +32,15 @@ const App = () => {
   const [number, setNumber] = useState<number>(0);
   const [userAnswers, setUserAnswers] = useState<AnswerObject[] | any>([]);
   const [gameOver, setGameOver] = useState<boolean>(true);
+  const [name, setName] = useState<string>('');
 
-  //start by fetching quiz data
+  // start by fetching quiz data
   useEffect(() => {
     setLoading(true);
     setGameOver(false);
 
     axios({
       method: 'get',
-      //baseURL: 'https://s3.eu-west-2.amazonaws.com',
-      //url: 'https://s3.eu-west-2.amazonaws.com/interview.mock.data/payload.json',
       url: 'https://react-typescript-quiz.free.beeceptor.com/api/quiz',
       headers: {
         'access-control-allow-origin': '*',
@@ -48,7 +48,6 @@ const App = () => {
       },
     })
       .then((response) => {
-        //console.log('response--->', response);
         const { data } = response;
         setQuizData(data);
         setHeading(data.name);
@@ -61,24 +60,18 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    // console.log('number--->', number);
-    // console.log('userAnswers.length--->', userAnswers.length);
     if (!gameOver && userAnswers.length === TOTAL_QUESTIONS) {
       setScreen('SCORE');
       setGameOver(true);
     }
   }, [userAnswers, gameOver]);
-  //console.log('screen--->', screen);
 
-  //restrict all handleClicks to be exclusively on HTMLButton elements
+  // restrict all handleClicks to be exclusively on HTMLButton elements
   const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {
-    // console.log('event.currentTarget.value--->');
-    // console.log(event.currentTarget.value);
-    console.log('checking answernow-->');
     if (!gameOver) {
-      //user answer
-      const answer = event.currentTarget.value === 'CORRECT' ? true : false;
-      //check answer with correct answer
+      // user answer
+      const answer = event.currentTarget.value === 'CORRECT';
+      // check answer with correct answer
       const correct = questions[number].is_correct === answer;
       const answerObject = {
         number: number + 1,
@@ -87,13 +80,9 @@ const App = () => {
         correct,
         correct_answer: questions[number].feedback,
       };
-      console.log('answerObject--->');
-      console.log({ ...answerObject });
-      //save answer in the array for user user answers
-      //@ts-ignore
+      // save answer in the array for user user answers
+      // @ts-ignore
       setUserAnswers((prev) => {
-        console.log('setUserAnswers--->');
-        console.log(prev.concat([answerObject]));
         return [...prev, answerObject];
       });
       setNumber(number + 1);
@@ -101,36 +90,26 @@ const App = () => {
   };
 
   const getQuestions = () => {
-    console.log('quizData--->', quizData);
-    console.log('activity--->preobelm--->', activity);
     const activityArray = _.filter(quizData.activities, {
       activity_name: activity,
     });
-    console.log('activityArray--->', activityArray);
-    //return the filtered array's first element (as there're only two activity elements)
-    const questions = activityArray[0].questions;
-    console.log('questions--->', questions);
+    // return the filtered array's first element (as there're only two activity elements)
+    const { questions } = activityArray[0];
     return sortArray(questions);
   };
 
-  const selectActivity = (event: React.MouseEvent<HTMLAnchorElement>) => {
+  const selectActivity = async (event: React.MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
-    console.log('event.selectActivity--->');
-    console.log(event.currentTarget.textContent);
     if (event.currentTarget.textContent !== null) {
       const activityWordsInArray = event.currentTarget.textContent.split(' ');
       if (activityWordsInArray.length > 0) {
         const selectedActivity = capitalCaseByWords(activityWordsInArray);
         if (!gameOver) {
-          console.log('selectedActivity--->after-capitalcase-->');
-          console.log(selectedActivity);
           setActivity(selectedActivity);
           const questions = getQuestions();
           setQuestions(questions);
           setNumber(number);
           setScreen('QUESTION');
-          console.log('selected activity questions --->');
-          console.log(getQuestions());
         }
       }
     }
@@ -146,6 +125,11 @@ const App = () => {
 
   return (
     <>
+      <p>
+        name is----
+        {name}
+        ---
+      </p>
       <GlobalStyle />
       {loading && <span>Loading...</span>}
 
@@ -154,9 +138,9 @@ const App = () => {
           heading={heading}
           activities={activities}
           selectActivity={selectActivity}
+          // selectActivity={() => setName('santhosh')}
         />
       )}
-      {/* {console.log('selectedActivity-->', activity)} */}
 
       {screen === 'QUESTION' && (
         <Questions
@@ -168,7 +152,6 @@ const App = () => {
         />
       )}
 
-      {/* {console.log('userAnswers--->', userAnswers)} */}
       {screen === 'SCORE' && (
         <Score
           activity={activity}
